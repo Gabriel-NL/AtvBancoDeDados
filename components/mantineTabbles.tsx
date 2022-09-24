837d792984303243269f4aa5fd7684b22eaad821
Gabriel Nogueira Lages <78828285+Gabriel-NL@users.noreply.github.com>
2022-09-24 20:48:26 -0300

    Molde feito

--- components/mantineTabbles.tsx
+++ components/mantineTabbles.tsx
@@ -4,11 +4,15 @@ import  styles  from  '../styles/Tables.module.css'
 
 function MantineTable() {
     return  <div className={styles.maincontent}>
+
     <h1>Tabela do tipo da despesa</h1>
+        <input type="text" placeholder='ID' />
+        <input type="text" placeholder='Nome do tipo da despesa' />
+        <input type="text" placeholder='Status (letra)' />  
     <Table>
       <thead> 
         <tr> 
-          <th>ID</th>
+          <th>ID tipo</th>
           <th>Despesa nome</th>
           <th>Status</th>
         </tr>
@@ -17,10 +21,13 @@ function MantineTable() {
     </Table>
 
     <h1>Tabela central dos custos</h1>
+        <input type="text" placeholder='ID' />
+        <input type="text" placeholder='Centro de custos' />
+        <input type="text" placeholder='Status (letra)' />
     <Table>
       <thead> 
         <tr> 
-          <th>ID</th>
+          <th>ID central</th>
           <th>Centro de custos</th>
           <th>Status</th>
         </tr>
@@ -28,6 +35,51 @@ function MantineTable() {
       <tbody>{}</tbody>
     </Table>
 
+    <h1>Forma de pagamento</h1>
+        <input type="text" placeholder='ID' />
+        <input type="text" placeholder='Nome' />
+        <input type="text" placeholder='Status (letra)' />
+    <Table>
+      <thead> 
+        <tr> 
+          <th>ID pagamento</th>
+          <th>Centro de custos</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>{}</tbody>
+    </Table>
+
+    <h1>Tabela Geral do financeiro</h1>
+        <input type="text" name="" placeholder='ID' />
+        <input type="text" name="" placeholder='Codigo do custo' />
+        <input type="text" name="" placeholder='Cod. tipo da despesa' />
+        <input type="text" name="" placeholder='Cod. forma. pagamento' />
+        <input type="text" name="" placeholder='Numero nota fiscal' />
+        <input type="text" name="" placeholder='Numero parcelas' />
+        <input type="text" name="" placeholder='Status (letra)' />
+        <input type="text" name="" placeholder='Despesa' />
+        <input type="text" name="" placeholder='Juros' />
+        <input type="text" name="" placeholder='Total despesa' />
+        <input type="text" name="" placeholder='Data de vencimento' />
+    <Table>
+      <thead> 
+        <tr> 
+          <th>ID</th>
+          <th>Cod centro de custos</th>
+          <th>Cod tipo despesa</th>
+          <th>Cod forma pagamento</th>
+          <th>Numero nota fiscal</th>
+          <th>Numero parcelas</th>
+          <th>Status</th>
+          <th>Valor despesa</th>
+          <th>Valor de juros</th>
+          <th>Valor total despesa</th>
+          <th>Data de vencimento</th>
+        </tr>
+      </thead>
+      <tbody>{}</tbody>
+    </Table>
 
     </div>;
 }
